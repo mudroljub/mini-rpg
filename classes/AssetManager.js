68c3283652fba4cbe25c9c065967a142c5e53ea2
Damjan Pavlica <mudroljub@gmail.com>
2019-08-30 10:59:45 +0200

    class AssetManager

--- classes/AssetManager.js
+++ classes/AssetManager.js
@@ -3,34 +3,33 @@ export const objects   = {}
 
 const loader = new THREE.JSONLoader()
 
-export default function AssetManager() {
-  this.TEXTURES  = {}
-  this.MATERIALS = {}
-  this.MESHES    = {}
-  this.ITEMS     = {}
-}
-
-AssetManager.prototype.loadMaterials = function(callback) {
-  for (let i = 0; i < IMAGES.length; i++) {
-    const texture = THREE.ImageUtils.loadTexture('assets/textures/' + IMAGES[i] + '.jpg')
-    texture.magFilter = THREE.NearestFilter
-    texture.minFilter = THREE.LinearMipMapLinearFilter
-    this.TEXTURES[IMAGES[i]] = texture
-    this.MATERIALS[IMAGES[i]] = new THREE.MeshLambertMaterial({map: texture })
-    this.MATERIALS[IMAGES[i]].dynamic = true
+export default class AssetManager {
+  constructor() {
+    this.TEXTURES  = {}
+    this.MATERIALS = {}
   }
-  return callback()
-}
 
-AssetManager.prototype.loadMeshes = function(meshes, callback) {
-  let i = 0
-  meshes.forEach(mesh => {
-    loader.load('assets/' + mesh + '.json', (geometry, materials) => {
-      const material = new THREE.MultiMaterial(materials)
-      objects[mesh] = new THREE.Mesh(geometry, material)
-      i++
-      if (i === meshes.length)
-        callback(objects)
+  loadMaterials(callback) {
+    for (let i = 0; i < IMAGES.length; i++) {
+      const texture = THREE.ImageUtils.loadTexture('assets/textures/' + IMAGES[i] + '.jpg')
+      texture.magFilter = THREE.NearestFilter
+      texture.minFilter = THREE.LinearMipMapLinearFilter
+      this.TEXTURES[IMAGES[i]] = texture
+      this.MATERIALS[IMAGES[i]] = new THREE.MeshLambertMaterial({map: texture })
+      this.MATERIALS[IMAGES[i]].dynamic = true
+    }
+    return callback()
+  }
+
+  loadMeshes(meshes, callback) {
+    let i = 0
+    meshes.forEach(mesh => {
+      loader.load('assets/' + mesh + '.json', (geometry, materials) => {
+        const material = new THREE.MultiMaterial(materials)
+        objects[mesh] = new THREE.Mesh(geometry, material)
+        i++
+        if (i === meshes.length) callback(objects)
+      })
     })
-  })
+  }
 }
